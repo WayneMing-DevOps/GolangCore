c6d04a33732ad8f354ac5593cf8ef10e7fed69ee
WayneMing-Bit01 <badmanweimin@gmail.com>
2025-06-02 11:32:21 +0800

    67

---
+++ src/code.wayneming.com/LeetCode/67/67.go
@@ -0,0 +1,18 @@
+/*
+给你两个二进制字符串 a 和 b ，以二进制字符串的形式返回它们的和。
+strconv.FormatInt(year, 2)
+strconv.ParseInt
+*/
+package main
+
+import "fmt"
+
+func addBinary(a string, b string) string {
+	fmt.Printf("%c", a)
+	return ""
+}
+
+func main() {
+	res := addBinary("123", "")
+	fmt.Println(res)
+}
