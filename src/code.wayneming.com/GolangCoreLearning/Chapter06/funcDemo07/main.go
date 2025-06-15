a893b8b63be3b4a947c381361c7e35586af73588
WayneMing-Bit01 <badmanweimin@gmail.com>
2025-06-15 10:52:24 +0800

    ArrayExercise1 创建一个byte类型的26个元素的数组，分别放置'A'-'Z',使用for循环访问所有元素并打印出来。提示：字符数据运算'A'+1='B'

--- src/code.wayneming.com/GolangCoreLearning/Chapter06/funcDemo07/main.go
+++ src/code.wayneming.com/GolangCoreLearning/Chapter06/funcDemo07/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 var age int = 10
 var Name string = "Wayne"
@@ -13,5 +16,12 @@ func test() {
 }
 
 func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // 监听并在 0.0.0.0:8080 上启动服务
 	test()
 }
